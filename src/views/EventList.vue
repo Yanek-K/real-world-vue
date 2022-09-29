635ca535d4e6abc862617daccc0497b3d39c7a4b
Yanek-K <yanek.dev@gmail.com>
2022-09-29 12:14:43 -0400

    Start Vuex

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,35 +1,33 @@
 <template>
+  <h1>Events for Good</h1>
   <div class="events">
-    <h1>Events For Good</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import EventCard from "@/components/EventCard.vue";
+import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
-
 export default {
-  name: "Home",
+  name: 'EventList',
   components: {
     EventCard
   },
   data() {
     return {
       events: null
-    };
+    }
   },
   created() {
     EventService.getEvents()
       .then(response => {
-        this.events = response.data;
+        this.events = response.data
       })
       .catch(error => {
-        console.log(error);
-      });
+        console.log(error)
+      })
   }
-};
+}
 </script>
 
 <style scoped>
